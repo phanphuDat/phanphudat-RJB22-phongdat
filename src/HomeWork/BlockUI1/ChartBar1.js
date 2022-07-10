5b742121b97389d369e29508f41c3234b20f1b8a
Mr.Dat <phongdat.20022000@gmail.com>
2022-07-10 13:47:53 +0700

    add

--- src/HomeWork/BlockUI1/ChartBar1.js
+++ src/HomeWork/BlockUI1/ChartBar1.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import './Chart1.css';
+import { AiFillStar } from "react-icons/ai"
 
-export default function ChartBar1() {
+const align = {alignItems: 'center'}
+
+export default function ChartBar1({ text = 'TEXT', color1 = 'violet', color = 'pink', percentage = 50 }) {
   return (
-    <div>ChartBar1</div>
+    <>
+        <div className="chart_Container">
+        {/* <div className="text1"> <AiFillStar/> </div> */}
+            <div className="column_left" style={{backgroundColor: color1}}>
+                <span className="text">{text}</span>
+            </div>
+            <div className="column_right">
+                <div className="column_percentage_left" style={{backgroundColor: color, width: `${percentage}%`}}>
+                    <span className="text">{percentage}%</span>
+                </div>
+                <div className="column_percentage_right"></div>
+            </div>
+        </div>
+    </>
   )
 }
--- src/HomeWork/index.js
+++ src/HomeWork/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import BoxProduct1 from './Basic1/BoxProduct1'
 import BoxProduct2 from './Basic2/BoxProduct2'
 import BoxProduct3 from './Basic3/BoxProduct3';
+import ChartBar1 from './BlockUI1/ChartBar1';
 
 const styleLine = {
     marginTop: '20px',
@@ -23,7 +24,13 @@ export default function index() {
 
         <div style={styleLine}> <hr/></div>
 
-        
+        <ChartBar1 text='BANDWIDTH' color1='#e54d4c' color='#f45456' percentage={20}/>
+        <ChartBar1 text='STORAGE' color1='#2db8cd' color='#31c8dd' percentage={50}/>
+        <ChartBar1 text='USERS' color1='#4ac25e' color='#51d567' percentage={70}/>
+        <ChartBar1 text='VISITORS' color1='#e9c318' color='#fcd518' percentage={30}/>
+        <ChartBar1 text='EMAILS' color1='#bb70d5' color='#cb79e7' percentage={45}/>
+        <ChartBar1 text='BASIC' color1='#eb59d6' color='#fe60e8' percentage={50}/>
+        <ChartBar1 text='OTHERS' color1='#5890da' color='#619cec' percentage={37}/>
     </div>
   )
 }
